e90914bf261e60a96774d68eb31ee4708502e888
Philip <philip@xsonline.eu>
2020-05-27 02:25:29 +0200

    updated settings

--- src/environments/environment.common.ts
+++ src/environments/environment.common.ts
@@ -8,8 +8,8 @@ export const environment: GlobalConfig = {
   // Angular Universal server settings.
   ui: {
     ssl: false,
-    host: 'dspace.poc.euraknos.cf',
-    port: 80,
+    host: 'localhost',
+    port: 4000,
     // NOTE: Space is capitalized because 'namespace' is a reserved string in TypeScript
     nameSpace: '/',
   },
