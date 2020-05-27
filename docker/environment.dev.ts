b31008895efda5c775925c314a03ec78ad3103bd
Philip <philip@xsonline.eu>
2020-05-27 02:17:14 +0200

    updated settings

--- docker/environment.dev.ts
+++ docker/environment.dev.ts
@@ -8,6 +8,14 @@
 import { GlobalConfig } from '../config/global-config.interface';
 
 export const environment: Partial<GlobalConfig> = {
+  // Angular Universal server settings.
+  ui: {
+    ssl: false,
+    host: 'dspace.poc.euraknos.cf',
+    port: 80,
+    // NOTE: Space is capitalized because 'namespace' is a reserved string in TypeScript
+    nameSpace: '/',
+  },
   rest: {
     ssl: false,
     host: 'api.dspace.poc.euraknos.cf',
